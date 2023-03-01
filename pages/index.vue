7998d941ca4268dfbeaee80445289677446e74ef
warflash <nwiesinger67@gmail.com>
2023-03-01 14:13:26 +0100

    init

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <img src="/logo.svg" />
+        <div>
+            <span>
+                Hello World
+            </span>
+            <NuxtLink to="/nested">-> Nested</NuxtLink>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/nested/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <img src="/logo.svg" />
+        <div>
+            <span>
+                Hellow from nested
+            </span>
+            <NuxtLink to="/">-> Home</NuxtLink>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+
+</style>
